f0ef3477b34fbc62acbdc9764dd0cdcd2405385a
Daniel Hart <daniel.17.hart@gmail.com>
2025-10-20 22:01:47 -0500

    Add drills to challenges; Add oncourt to learning section; Setup ios application

---
+++ skills-ios/skills-ios/skills_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  skills_iosApp.swift
+//  skills-ios
+//
+//  Created by Daniel Hart on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct skills_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
